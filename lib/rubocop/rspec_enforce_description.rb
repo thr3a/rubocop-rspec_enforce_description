a0a1326564103c841a26e3bde867a9542c062321
thr3a <thr3a@example.com>
2025-05-22 23:29:44 +0900

    init

---
+++ lib/rubocop/rspec_enforce_description.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "rspec_enforce_description/version"
+
+module Rubocop
+  module RspecEnforceDescription
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
